8866d39cb47ad0b41817b0c3ea5abb1e6a3e5618
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-03-25 19:26:21 +0500

    init commit

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+char repeat = 'y';
+double numberA = 0;
+double numberB = 0;
+double result = 0;
+while (repeat == 'y')
+{
+    Console.WriteLine("Введите число А");
+    numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    numberB = Convert.ToInt32(Console.ReadLine());
+    result = Math.Pow(numberA, numberB);
+    System.Console.WriteLine($"А в степени В = {result}");
+    System.Console.WriteLine("Посчитаем еще пару чисел? (y - да, n - нет)");
+    repeat = char.ToLower(Convert.ToChar(Console.ReadLine()));
+}
---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из N(-100 100) рандомных элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+System.Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+Console.Write("[");
+for (int i = 0; i < array.Length - 1; i++)
+{
+    System.Console.Write(array[i] + ", ");
+}
+Console.Write(array[array.Length - 1] + "]");
